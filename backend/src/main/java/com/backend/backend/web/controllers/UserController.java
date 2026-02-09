0e6bf373b9aaebecc98be8877d7bcd5644b9c6c6
Amir Hurtado <a.hurtado@utp.edu.co>
2026-02-09 15:02:38 -0500

    client setup

--- backend/src/main/java/com/backend/backend/web/controllers/UserController.java
+++ backend/src/main/java/com/backend/backend/web/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.backend.backend.web.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@CrossOrigin(origins="http://localhost:4200/")
 public class UserController {
 
     private final UserService userService;
